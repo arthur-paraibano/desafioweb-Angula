85d6252b261e92282f41c90f5b4cadf6cc6b2027
arthur-paraibano <arthurmik860@gmail.com>
2024-11-07 16:49:54 -0300

    criando novos componentes

---
+++ src/app/components/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'menu-component',
+    template: '<h2>Arthur</h2>',
+    styles: ['h2 {color: blue}'],
+
+})
+
+export class MenuComponet{
+
+}

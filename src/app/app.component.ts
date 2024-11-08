81f96d1092a7ab3fbabaee419a400573824b6cc0
arthur-paraibano <arthurmik860@gmail.com>
2024-11-08 15:40:03 -0300

    novos componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, VERSION } from '@angular/core';
-import { HelloComponent } from "./hello.component";
+import { HelloComponent } from './hello.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [HelloComponent]
+  styleUrls: ['./app.component.scss'],
+  imports: [HelloComponent],
 })
 export class AppComponent {
   name = 'tarefas' + VERSION.major;
---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+// src/app/models/usuario.model.ts
+export interface Usuario {
+    id: number;
+    nome: string;
+    email: string;
+    dataNascimento: Date;
+  }
+  

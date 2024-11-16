60dc5c2eaa306a4d98a1fce2757beba1fe64ad38
arthur-paraibano <arthurmik860@gmail.com>
2024-11-16 18:08:48 -0300

    importação de modulos em app.routes.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
